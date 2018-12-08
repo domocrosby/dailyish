6524851374f695afdf8b407b7cfb47fa2671ff21
Dominic <domcrosby1@gmail.com>
2018-12-08 07:52:21 -0800

    Started out

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from './src/components/header';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Header headerText={'Dailyish'} />
+    );
+  }
+}
